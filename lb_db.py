74b2e12e4f0cd78174e6b4dcc95aef78f40f93d9
Craig Dalton <craig@loudsquel.ch>
2019-10-06 22:12:56 +0100

    Fixed rather a lot

--- lb_db.py
+++ lb_db.py
@@ -12,8 +12,7 @@ sqlite3.register_converter("json", json.loads)
 # Table containing basic LB infomation
 SQL_LB_TABLE = """
 CREATE TABLE IF NOT EXISTS lb (
-    id integer PRIMARY KEY NOT NULL,
-    mgmt_ip text NOT NULL,
+    id text PRIMARY KEY,
     hostname text NOT NULL,
     vendor text NOT NULL,
     version text NOT NULL
@@ -24,11 +23,10 @@ CREATE TABLE IF NOT EXISTS lb (
 # may be required
 SQL_F5_DETAIL_TABLE = """
 CREATE TABLE IF NOT EXISTS f5_detail (
-    id integer PRIMARY KEY,
-    lb_id integer NOT NULL,
+    lb_id text PRIMARY KEY,
     frontend_ip json,
     backend_ip json,
-    FOREIGN KEY (lb_id) REFERENCES lb (mgmt_ip)
+    FOREIGN KEY (lb_id) REFERENCES lb (id)
 );"""
 
 
@@ -81,7 +79,7 @@ if __name__ == "__main__":
     parser.add_argument("--path", type=str, required=True)
     args = parser.parse_args()
 
-    conn = sqlite3.connect(args.path)
+    conn = connect(args.path)
 
     with conn:
         # create projects table
@@ -92,15 +90,15 @@ if __name__ == "__main__":
 
         # Add record 1
         lb = {
+            "id": "10.55.137.24",
             "vendor": "F5",
-            "mgmt_ip": "10.55.137.24",
             "hostname": "bigip-01.customer1.local",
             "version": "10",
         }
-        lb_id = insert(conn, "lb", **lb)
+        insert(conn, "lb", **lb)
         # create detail record
         f5_detail = {
-            "lb_id": lb_id,
+            "lb_id": lb["id"],
             "frontend_ip": ["205.134.12.57"],
             "backend_ip": [
                 "10.55.136.100",
@@ -109,35 +107,35 @@ if __name__ == "__main__":
                 "10.55.136.103",
             ],
         }
-        lb_id = insert(conn, "f5_detail", **f5_detail)
+        insert(conn, "f5_detail", **f5_detail)
 
         # Add record 2
         lb = {
+            "id": "10.83.121.250",
             "vendor": "F5",
-            "mgmt_ip": "10.83.121.250",
             "hostname": "my-load-balancer.customer2.internal",
             "version": "11",
         }
-        lb_id = insert(conn, "lb", **lb)
+        insert(conn, "lb", **lb)
         # create detail record
         f5_detail = {
-            "lb_id": lb_id,
+            "lb_id": lb["id"],
             "frontend_ip": ["69.254.121.36"],
             "backend_ip": ["10.83.121.10", "10.83.121.11"],
         }
-        lb_id = insert(conn, "f5_detail", **f5_detail)
+        insert(conn, "f5_detail", **f5_detail)
 
         # Add record 3
         lb = {
+            "id": "10.55.12.30",
             "vendor": "F5",
-            "mgmt_ip": "10.55.12.30",
             "hostname": "big-ip.customer3.com",
             "version": "10",
         }
-        lb_id = insert(conn, "lb", **lb)
+        insert(conn, "lb", **lb)
         # create detail record
         f5_detail = {
-            "lb_id": lb_id,
+            "lb_id": lb["id"],
             "frontend_ip": ["220.242.72.34"],
             "backend_ip": [
                 "10.55.13.44",
@@ -146,14 +144,14 @@ if __name__ == "__main__":
                 "10.55.13.47",
             ],
         }
-        lb_id = insert(conn, "f5_detail", **f5_detail)
+        insert(conn, "f5_detail", **f5_detail)
 
         # Select data with relationships resolved
         sql = """
         SELECT
+            lb.id as 'IP ADDRESS',
             lb.hostname as HOSTANME,
             lb.vendor as VENDOR,
-            lb.mgmt_ip as 'IP ADDRESS',
             f5_detail.frontend_ip as 'FRONTEND POOL',
             f5_detail.backend_ip as 'BACKEND POOL'
         FROM lb LEFT JOIN f5_detail
@@ -168,6 +166,7 @@ if __name__ == "__main__":
         for row in rows:
             i = 0
             for column in row:
-                print(column_names[i], column, sep=": ")
+                desc = f"{column_names[i]} ({type(column).__name__})"
+                print(f"{desc:>20}: {column}")
                 i += 1
             print("")
